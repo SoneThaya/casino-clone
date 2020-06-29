e7c269fd604cab051be009fa2f84ac1e81c80be0
Sone <sthaya@gmail.com>
2020-06-28 17:37:34 -0700

    create navbar component and styled

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+      <div className='container'>
+        <Link to='/'>
+          <h2>React Blog</h2>
+        </Link>
+     
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;

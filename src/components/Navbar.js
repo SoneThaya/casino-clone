661e0037abd653884f8130d66d81f1505772b9cf
Sone <sthaya@gmail.com>
2020-06-28 17:44:13 -0700

    installed styled components, added links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {NavStyles} from '../styles/Styles'
 
 const Navbar = () => {
   return (
-    <div className='nav'>
+    <NavStyles>
       <div className='container'>
         <Link to='/'>
-          <h2>React Blog</h2>
+          <h1>Casino</h1>
         </Link>
-     
+        <nav>
+            <Link to='/casino'>Casino</Link>
+            <Link to='/club'>Club</Link>
+            <Link to='/promotions'>Promotions</Link>
+            <Link to='/entertainment'>Entertainment</Link>
+            <Link to='/dining'>Dining</Link>
+            <Link to='/golf'>Golf</Link>
+          </nav>
       </div>
-    </div>
+    </NavStyles>
   )
 }
 
